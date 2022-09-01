a922799136e6f68175e41d24eb24815f0c40ac57
Eduard <ripmag@gmail.com>
2022-09-01 14:27:14 +0200

    feat: create deviceController

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,14 +1,36 @@
-class TypeController {
-    async create(req,res){
+const uuid = require('uuid') //генерация уникальных ID шников
+const path = require('path')
+const { Device } = require('../models/models')
+
+const ApiError = require('../error/ApiError')
+
+class DeviceController {
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId, info } = req.body
+
+            const { img } = req.files //используем express-fileupload
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, "..", "static", fileName))
+
+            const device = await Device.create({ name, price, brandId, typeId, img: fileName })
+
+            return res.json(device)
+        }
+        catch (e) {
+            next(ApiError.badRequest(e.message))
+
+        }
+
 
     }
-    
-    async getAll(req,res){
-        
-    }    
-    async getOne(req,res){
-        
-    }    
+
+    async getAll(req, res) {        
+
+    }
+    async getOne(req, res) {
+
+    }
 }
 
-module.exports = new TypeController()
+module.exports = new DeviceController()
--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const models = require('./models/models')
 const PORT = process.env.PORT
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname,'static')))
+app.use(fileUpload({}))
 app.use('/api',router)
 
 
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
